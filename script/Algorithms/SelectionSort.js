7dbcab9485c03f731533c8e586d4f9546ee6e43d
Syed Sadat Ali <sadat3332@gmail.com>
2024-06-15 19:33:52 +0530

    swapBars added for selection sort

--- script/Algorithms/SelectionSort.js
+++ script/Algorithms/SelectionSort.js
@@ -1,6 +1,6 @@
-import { sleep,swap } from "../utils.js";
+import { sleep, swap } from "../utils.js";
 
-export async function selectionSort(bars,sleepTime) {
+export async function selectionSort(bars, sleepTime) {
     for (let i = 0; i < bars.length; i++) {
         let minIndex = i;
         for (let j = i + 1; j < bars.length; j++) {
@@ -14,18 +14,32 @@ export async function selectionSort(bars,sleepTime) {
             bars[j].barElement.style.backgroundColor = 'blue';
             bars[minIndex].barElement.style.backgroundColor = 'blue';
         }
-        bars[i].barElement.style.backgroundColor = 'purple';
+        bars[i].barElement.style.backgroundColor = 'red';
         bars[minIndex].barElement.style.backgroundColor = 'green';
-        swap(i, minIndex,bars);
-        
-        await sleep(100);
-        bars[i].barElement.style.height = bars[i].height + '%';
-        bars[minIndex].barElement.style.height = bars[minIndex].height + '%';
+        bars[i].barElement.style.transition = 'height 0.3s ease';
+        bars[minIndex].barElement.style.transition = 'height 0.3s ease'; // transition while swapping bars
+
+        await swapBars(bars, i, minIndex);
         bars[i].barElement.style.backgroundColor = 'blue';
         bars[minIndex].barElement.style.backgroundColor = 'blue';
+        bars[i].barElement.style.transition = '';
+        bars[minIndex].barElement.style.transition = '';
     }
     for (let i = 0; i < bars.length; i++) {
         bars[i].barElement.style.backgroundColor = 'green';
         await sleep(3);
+    }
+}
+
+async function swapBars(bars, i, minIndex) {
+    if (i !== minIndex) {
+        let tempHeight = bars[i].height;
+        bars[i].height = bars[minIndex].height;
+        bars[minIndex].height = tempHeight;
+
+        bars[i].barElement.style.height = bars[i].height + '%';
+        bars[minIndex].barElement.style.height = bars[minIndex].height + '%';
+
+        await sleep(300); 
     }
 }
